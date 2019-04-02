2def731d2924726acc274d6215b46d760096a29a
Cristóbal Medina <48692695+krztvlmedina@users.noreply.github.com>
2019-04-01 21:20:24 -0300

    Archivos finales
    SeguidorDeLinea2.java es el definitivo para sacarnos un puntaje interestelar.
    
    test.java es para ver si podemos ponerle la música de kirby.

--- SeguidorDeLinea2.java
+++ SeguidorDeLinea2.java
@@ -1,6 +1,7 @@
 import lejos.nxt.*;
+import lejos.util.Delay;
 //config ganadora= delta = 300, baseSpeed = 0
-public class SeguidorDeLinea2 {
+public class SeguidorDeLinea2{
 
     //variables y objetos recurrentes
     static TouchSensor sensorTacto = new TouchSensor(SensorPort.S2);
@@ -9,17 +10,20 @@ public class SeguidorDeLinea2 {
     LightSensor sensorLuz = new LightSensor(SensorPort.S1);
     int baseSpeed = 0; //velocidad crucero con que avanza el robot
 
-    /*public void linea(int distancia){ //avanza rotaciones en ángulos i.e 1 rot = 360
-        motorIzquierdo.rotateTo(motorIzquierdo.getTacho()-distancia, true);
+    public void linea(int distancia){ //avanza rotaciones en ángulos i.e 1 rot = 360
+        motorIzquierdo.rotate(distancia, true);
         motorDerecho.rotate(distancia);
     }
-	*/
+	
 
-    /*public void girarEnEje(int angulo){ //gira en su propio eje
+    public void girarEnEje(int angulo){ //gira en su propio eje
         motorIzquierdo.rotate(-angulo, true);
         motorDerecho.rotate(angulo);
-    }*/
-
+    }
+	public void setearVelocidad(int velocidad){
+		motorDerecho.setSpeed(velocidad);
+		motorIzquierdo.setSpeed(velocidad);
+	}
     public void setearGiro(int valor){ //setear la velocidad de giro en el eje y comienza el movimiento
         motorIzquierdo.setSpeed(valor);
         motorDerecho.setSpeed(valor);
@@ -38,19 +42,40 @@ public class SeguidorDeLinea2 {
     }
 
     public int calibrar(){ //le asigna valores a blanco y negro
-		System.out.println("Posicione en blanco para medir");
+        System.out.println("Posicione en blanco para medir");
         Button.ENTER.waitForPress(); //espera a presionar enter para calibrar
         int  blanco = sensorLuz.readValue();
-		System.out.println("Valor blanco :"+blanco+". Posicione sobre banda negra para medir");
+        System.out.println("Valor blanco :"+blanco+". Posicione sobre banda negra para medir");
         Button.ENTER.waitForPress();
         int  negro = sensorLuz.readValue();
-		System.out.println("Valor negro: "+negro);
+        System.out.println("Valor negro: "+negro);
         return (blanco+negro)/2;
     }
+	public void musica(){
+		Sound.setVolume(100);
+		  Sound.playTone(523,70);  // play tone according to light sensor readings
+		  Delay.msDelay(170);                                        // wait 200 milliseconds before checking again
+		  Sound.playTone(523,70);  // play tone according to light sensor readings
+		  Delay.msDelay(170);  
+		  Sound.playTone(523,70);  // play tone according to light sensor readings
+		  Delay.msDelay(170);  
+		  Sound.playTone(523,70);  // play tone according to light sensor readings
+		  Delay.msDelay(500);  
+		  Sound.playTone(415,70);  // play tone according to light sensor readings
+		  Delay.msDelay(500);  
+		  Sound.playTone(466,70);  // play tone according to light sensor readings
+		  Delay.msDelay(500);  
+		  Sound.playTone(523,70);  // play tone according to light sensor readings
+		  Delay.msDelay(400);  
+		  Sound.playTone(466,70);  // play tone according to light sensor readings
+		  Delay.msDelay(170);  
+		  Sound.playTone(523,70);  // play tone according to light sensor readings
+		  Delay.msDelay(200);  
+	}
 
     public void run() {
         Button.ESCAPE.addButtonListener(new ButtonListener() {  //listener de botón para detectar cuándo se
-                                                                //presiona el botón ESCAPE
+            //presiona el botón ESCAPE
             @Override
             public void buttonPressed(Button b) {
                 System.exit(0); //termina el programa cuando ESCAPE es presionado
@@ -60,48 +85,53 @@ public class SeguidorDeLinea2 {
             public void buttonReleased(Button b) {
             }
         });
-		System.out.println("Presione ENTER para comenzar.");
+		
+        System.out.println("Presione ENTER para comenzar.");
         Button.ENTER.waitForPress();
         int luz = sensorLuz.readValue();
-		int promedio = calibrar()+2;
-		//agregar delay
-		System.out.println("Calibracion efectuada, promedio: "+promedio+". Presione ENTER para empezar el recorrido.");
-		
+        int promedio = calibrar()+2;
+        //agregar delay
+        System.out.println("Calibracion efectuada, promedio: "+promedio+". Presione ENTER para empezar el recorrido.");
+
         Button.ENTER.waitForPress();
-		int delta=300;
+        int delta=300;
         while (!sensorTacto.isPressed()) { //while el boton no está apretado
-			motorDerecho.forward();
-			motorIzquierdo.forward();
+            motorDerecho.forward();
+            motorIzquierdo.forward();
+            if (luz <= promedio) //está más cerca del negro
+                virarIzquierda(delta);
+                //hacer la velocidad basevariable cuando la dif es muy chica, pq avanza lento cuando se queda en línea recta final
+            else
+                virarDerecha(delta); //está hacia lo blanco
+
+            luz = sensorLuz.readValue();
+        } //hasta aqui sigue la linea
+		motorDerecho.stop();
+		motorIzquierdo.stop();
+		setearVelocidad(500);
+		linea(-150);
+        girarEnEje(300);
+		
+        virarIzquierda(delta);
+		
+        while (!sensorTacto.isPressed()) { //while el boton no está apretado
+		motorDerecho.forward();
+        motorIzquierdo.forward();
             if (luz <= promedio) //está más cerca del negro
                 virarDerecha(delta);
-				//hacer la velocidad basevariable cuando la dif es muy chica, pq avanza lento cuando se queda en línea recta final
             else
                 virarIzquierda(delta); //está hacia lo blanco
 			
             luz = sensorLuz.readValue();
-        } //hasta aqui sigue la linea
+        }
+		
+		motorDerecho.stop();
 		motorIzquierdo.stop();
-        motorDerecho.stop();
+		Delay.msDelay(100);
+		musica();
 		
-        setearGiro(200); //empieza a girar (no gira porque está en negro ARREGLAR, agregar rotación mínima para encaminarse bien)
-		
-        while (luz > promedio) {
-            luz = sensorLuz.readValue();           
-            motorIzquierdo.stop();
-            motorDerecho.stop();
-		}
-		virarDerecha(delta);
-		while (!sensorTacto.isPressed()) { //while el boton no está apretado
-		motorDerecho.forward();
-		motorIzquierdo.forward();
-		if (luz <= promedio) //está más cerca del negro
-			virarDerecha(delta);
-		else
-			virarIzquierda(delta); //está hacia lo blanco
-		luz = sensorLuz.readValue();
-		}
-	}
-        
+    }
+
 
     public static void main (String [] args) {
         new SeguidorDeLinea2().run();
